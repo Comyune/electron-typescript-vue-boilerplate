924991f9fa1ca2177765d1836d5bebb8962d324b
Comyune Technologies <comyune@gmail.com>
2020-07-13 20:06:31 +0100

    Clean up redundant boilerplate

--- src/background.ts
+++ src/background.ts
@@ -9,13 +9,11 @@ const isDevelopment = process.env.NODE_ENV !== 'production'
 // be closed automatically when the JavaScript object is garbage collected.
 let win: BrowserWindow | null
 
-// Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
-  { scheme: 'app', privileges: { secure: true, standard: true } }
+  { scheme: 'seewhy', privileges: { secure: true, standard: true } }
 ])
 
 function createWindow () {
-  // Create the browser window.
   win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -42,29 +40,20 @@ function createWindow () {
   })
 }
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow()
   }
 })
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
   if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
     try {
       await installExtension(VUEJS_DEVTOOLS)
     } catch (e) {
@@ -74,7 +63,6 @@ app.on('ready', async () => {
   createWindow()
 })
 
-// Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === 'win32') {
     process.on('message', (data) => {
---
+++ tests/unit/views/home.spec.ts
@@ -0,0 +1,8 @@
+import { shallowMount } from '@vue/test-utils'
+import Home from '@/views/Home.vue'
+
+describe('Home.vue', () => {
+  it('renders', () => {
+    shallowMount(Home)
+  })
+})
